54735ae3e2c663bee8f3376443f8b2dca833c85c
jerryhanwei <jerryhanwei@163.com>
2020-06-18 22:54:33 +0800

    Initial commit

---
+++ src/main/java/com/han/model/User.java
@@ -0,0 +1,31 @@
+package com.han.model;
+
+public class User {
+    public String id;
+    public String address;
+    public String username;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+}
--- src/test/java/com/han/TesthanApplicationTests.java
+++ src/test/java/com/han/TesthanApplicationTests.java
@@ -1,9 +1,11 @@
 package com.han;
 
 import org.junit.jupiter.api.Test;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@MapperScan(basePackages = "com.han")
 class TesthanApplicationTests {
 
     @Test
