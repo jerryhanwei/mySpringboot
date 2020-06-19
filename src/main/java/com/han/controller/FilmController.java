7e32d6024515e3bf726d040d51758d12992271cf
jerryhanwei <jerryhanwei@163.com>
2020-06-19 17:08:47 +0800

    Initial commit

--- src/main/java/com/han/controller/FilmController.java
+++ src/main/java/com/han/controller/FilmController.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import javax.annotation.Resource;
 import javax.sql.DataSource;
 
 @Controller
--- src/main/java/com/han/service/impl/FilmService.java
+++ src/main/java/com/han/service/impl/FilmService.java
@@ -6,8 +6,6 @@ import com.han.service.IFilmService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import javax.annotation.Resource;
-
 @Service
 public class FilmService implements IFilmService {
     @Autowired
